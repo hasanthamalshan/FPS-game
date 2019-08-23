21c4457b60584ea416c838f0315a9063364c339a
Hasantha malshan <2017cs006@stu.ucsc.cmb.ac.lk>
2019-08-24 00:11:17 +0530

    gunfire with raycast

---
+++ Assets/Scripts/HandGunDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGunDamage : MonoBehaviour {
+
+	//int DamageAmount = 5;
+	//float TargetDistance;
+	float AllowedRange = 15;
+
+	public Camera fpsCam;
+	
+
+	void Update (){
+		RaycastHit Hit;
+		if(Input.GetMouseButtonDown(0)){
+			if(Physics.Raycast(fpsCam.transform.position , fpsCam.transform.forward , out Hit , AllowedRange)){
+
+					Debug.Log(Hit.transform.name);
+
+			}
+		}
+	}
+}
