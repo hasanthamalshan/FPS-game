a35b5f1e0b83b3a03662fda1df607084ee589e13
Hasantha malshan <2017cs006@stu.ucsc.cmb.ac.lk>
2019-08-23 15:44:39 +0530

    added gun script and some animations

---
+++ Assets/Scripts/GunFire.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour {
+	public AudioSource gunsound;
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			gunsound.Play();
+			GetComponent<Animation>().Play();
+			
+		}
+	}
+}
