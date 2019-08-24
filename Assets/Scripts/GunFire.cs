eb2e3fb4b407ac1a9c1e0952f81b9352fb1fdf86
Hasantha malshan <2017cs006@stu.ucsc.cmb.ac.lk>
2019-08-25 02:12:24 +0530

    reload and door animation
    added reload and door animation with door open button

--- Assets/Scripts/GunFire.cs
+++ Assets/Scripts/GunFire.cs
@@ -6,23 +6,34 @@ public class GunFire : MonoBehaviour {
 	public AudioSource gunsound;
 	public AudioSource reloadsound;
 
+	public static bool isrealoading = false;
+
 	void Update () {
-		if(Input.GetMouseButtonDown(0)){
+		if(Input.GetMouseButtonDown(0) && isrealoading == false){
 			if(ammoCollect.ammoLoad > 0){
 				gunsound.Play();
-				GetComponent<Animation>().Play();
+				GetComponent<Animation>().Play("GunShot");
 				ammoCollect.ammoLoad--;
 			}
 			
 		}
 		if(Input.GetKeyDown("r")){
 			if(ammoCollect.ammo > 0){
+				StartCoroutine("ReloadWait",1.0f);
 				reloadsound.Play();
-				//GetComponent<Animation>().Play();
+				GetComponent<Animation>().Play("gunReload");
 				ammoCollect.ammo--;
-				ammoCollect.ammoLoad = 10;
-			}
-			
+				ammoCollect.ammoLoad = 10;	
+			}	
 		}
+	}
+	IEnumerator ReloadWait(float time){
+
+		isrealoading = true ; 
+
+		yield return new WaitForSeconds(time);
+
+		isrealoading = false;
+		
 	}
 }
---
+++ Assets/Scripts/PlayerInstructions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerInstructions : MonoBehaviour
+{
+    [SerializeField] Text openDoor;
+    [SerializeField] GameObject Door;
+    public Camera fpsCam;
+
+    float AllowedRange = 1.5f;
+
+    void Update()
+    {
+        RaycastHit Hit;
+		
+		if(Physics.Raycast(fpsCam.transform.position , fpsCam.transform.forward , out Hit , AllowedRange)){
+				if(Hit.transform.tag == "doorButton1"){
+                    openDoor.text = "Press E to Open Door";
+                    if(Input.GetKeyDown("e")){
+                         Door.GetComponent<Animation>().Play();
+                    }
+			    }
+		}else{
+                    openDoor.text = "";
+                }
+    }
+}
