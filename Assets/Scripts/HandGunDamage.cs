9e3ccb51d7495f422ad9ba671a97a4eb0c6f9423
Hasantha malshan <hasanthamalshan07@gmail.com>
2019-08-30 22:57:38 +0530

    added bullet holes

--- Assets/Scripts/HandGunDamage.cs
+++ Assets/Scripts/HandGunDamage.cs
@@ -8,12 +8,18 @@ public class HandGunDamage : MonoBehaviour {
 	float AllowedRangem9 = 15;
 
 	public Camera fpsCam;
+
+	RaycastHit bullet;
+	[SerializeField] GameObject bullethole;
 	
 
 	void Update (){
 		RaycastHit Hit;
 		if(Input.GetMouseButtonDown(0) && CollectAmmoM9.ammoLoadM9 >0){
 			if(Physics.Raycast(fpsCam.transform.position , fpsCam.transform.forward , out Hit , AllowedRangem9)){
+				if(Physics.Raycast(fpsCam.transform.position , fpsCam.transform.forward , out bullet)){
+						Instantiate(bullethole,bullet.point,Quaternion.LookRotation(Vector3.up,bullet.normal));
+					}
 				if(Hit.transform.tag == "enemy"){
 					Hit.transform.SendMessage("deductPoints",DamageAmount);	
 				}
--- Assets/Scripts/MachineGunDamage.cs
+++ Assets/Scripts/MachineGunDamage.cs
@@ -8,12 +8,17 @@ public class MachineGunDamage : MonoBehaviour
 	float AllowedRangemp5k = 30;
 
 	public Camera fpsCam;
+	RaycastHit bullet;
+	[SerializeField] GameObject bullethole;
 	
 
 	void Update (){
 		RaycastHit Hit;
 		if(Input.GetMouseButton(0) && ammoCollect1.ammoLoadMP5K >0){
 			if(Physics.Raycast(fpsCam.transform.position , fpsCam.transform.forward , out Hit , AllowedRangemp5k)){
+				if(Physics.Raycast(fpsCam.transform.position , fpsCam.transform.forward , out bullet)){
+						Instantiate(bullethole,bullet.point,Quaternion.LookRotation(Vector3.up,bullet.normal));
+					}
 				if(Hit.transform.tag == "enemy"){
 					Hit.transform.SendMessage("deductPoints",DamageAmount);	
 				}
